c2d0e1213c7f41cc328605951b2632554a1ea5ea
WilliamSpencer <william.spencer@ucalgary.ca>
2022-11-23 03:00:28 +0000

    savaData button doesn't seem to run saveData method.

---
+++ src/application/Nutrients.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Nutrients {
+	
+
+	public Nutrients() {
+		int[] array = new int[5];
+	}
+
+}
--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -3,16 +3,41 @@ package application;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	
+	@FXML
+    private TextField breakfastName;
+	
+	@FXML
+    private TextField lunchName;
+	
+	@FXML
+    private TextField dinnerName;
+	
+	@FXML
+    private TextField snack1Name;
+	
+	@FXML
+    private TextField snack2Name;
+	
+	@FXML
+    private TextField breakfastCalories;
+	
+	@FXML
+	private Label averageCalories;
+
 
 	public void switchToMainScreen(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
@@ -34,6 +59,23 @@ public class SceneController {
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+	}
+	public void saveData(ActionEvent event) {
+		System.out.println("SaveData button works");
+		String[] nameList =  new String[5];
+		String[] caloriesList =  new String[5];
+		String[] saltList =  new String[5];
+		String[] fatList =  new String[5];
+		
+		nameList[0] = breakfastName.getText();
+		nameList[1] = lunchName.getText();
+		nameList[2] = dinnerName.getText();
+		nameList[3] = snack1Name.getText();
+		nameList[4] = snack2Name.getText();
+		
+		caloriesList[0] = breakfastCalories.getText(); 
+		System.out.println("" + caloriesList[0]);
+		averageCalories.setText(caloriesList[0]);
 	}
 
 }
