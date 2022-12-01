691928d51050740bd0ae8d29a1170005346f1de4
WilliamSpencer <william.spencer@ucalgary.ca>
2022-12-01 01:34:01 +0000

    Finished Meals Constructor.

--- src/application/Meals.java
+++ src/application/Meals.java
@@ -1,10 +1,53 @@
 package application;
 
-public class Meals extends Meal {
+import java.util.ArrayList;
 
-	public Meals(String aName) {
+public class Meals extends Meal {
+	private String name;
+	private ArrayList<Meal> Meals = new ArrayList<Meal>();
+	
+	
+	public Meals(String aName, Meal breakfast, Meal lunch, Meal dinner, Meal snack1, Meal snack2) {
+		
+		//creating new variables for encapsulation purposes
+		String mealsName = new String();
+		Meal breakfastMeal = new Meal();
+		Meal lunchMeal = new Meal();
+		Meal dinnerMeal = new Meal();
+		Meal snack1Meal = new Meal();
+		Meal snack2Meal = new Meal();
+		
+		//assigning encapsulation variables
+		mealsName = aName;
+		breakfastMeal = breakfast;
+		lunchMeal = lunch;
+		dinnerMeal = dinner;
+		snack1Meal = snack1;
+		snack2Meal = snack2;
+		
+		//assigning instance variables
+		name = mealsName;
+		Meals.add(breakfastMeal);
+		Meals.add(lunchMeal);
+		Meals.add(dinnerMeal);
+		Meals.add(snack1Meal);
+		Meals.add(snack2Meal);
 	}
 	
-	public 
+	public double AverageCalories() {
+		return 0;
+	}
+	
+	public double HighCalories() {
+		return 0;
+	}
+	
+	public double LowCalories() {
+		return 0;
+	}
+	
+	public double TotalCalories() {
+		return 0;
+	}
 
 }
